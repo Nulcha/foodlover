57950f873473f867cd21588466fe2b73739dedb2
Nulcha <turdiqulovelbek2@gmail.com>
2025-01-13 19:51:17 +0500

    js ga uzgartirish kiritildi

--- js/script.js
+++ js/script.js
@@ -8,13 +8,16 @@ setInterval(function(){
     //country---------------------------------------------------------------------------------
     const tabheader = document.querySelector(".tabheader__items");
     let mamlakat = document.querySelector('.country');
-    tabheader.addEventListener('click', (events) =>{
-        if(events.target.matches('.tabheader__item'))
-            tabheader.querySelectorAll('.tabheader__item').forEach(item => item.classList.remove('tabheader__item_active'))
+    tabheader.addEventListener('click', (events) => {
+        if (events.target.matches('.tabheader__item')) {
+            tabheader.querySelectorAll('.tabheader__item').forEach(item => item.classList.remove('tabheader__item_active'));
             events.target.classList.add('tabheader__item_active');
             const text = events.target.textContent.trim(); 
-             mamlakat.textContent = text;
+            mamlakat.textContent = text;
+        }
     });
+    
+    
     //QOLGAN VAQT
     
     const day = document.querySelector("#days");
