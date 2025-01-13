9436755f900025539f9c22d7c9f5ac74ae8c55ef
Nulcha <turdiqulovelbek2@gmail.com>
2025-01-13 19:16:09 +0500

    js

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ setInterval(function(){
     const mainContent = document.querySelector(".mainBox");
     loading.style.display = "none";
     mainContent.style.display = "block";
-    }, 3000);
+    }, 1000);
     //country---------------------------------------------------------------------------------
     const tabheader = document.querySelector(".tabheader__items");
     let mamlakat = document.querySelector('.country');
@@ -13,7 +13,7 @@ setInterval(function(){
             tabheader.querySelectorAll('.tabheader__item').forEach(item => item.classList.remove('tabheader__item_active'))
             events.target.classList.add('tabheader__item_active');
             const text = events.target.textContent.trim(); 
-            mamlakat.textContent = text;
+             mamlakat.textContent = text;
     });
     //QOLGAN VAQT
     
